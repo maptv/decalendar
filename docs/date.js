a0350aae883407d5fa95411ebbce83d3e0d7a2e3
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2023-08-07 21:44:49 -0400

    simple javascript implementation of the 3 dayOfThe (dot, .) date formats

--- docs/date.js
+++ docs/date.js
@@ -1,20 +1,31 @@
-const now = new Date(),
-    year = now.getUTCFullYear(),
-    month = now.getUTCMonth(),
-    day = now.getUTCDate(),
-    hour = now.getUTCHours(),
-    minute = now.getUTCMinutes(),
-    second = now.getUTCSeconds(),
-    millisecond = now.getUTCMilliseconds(),
-    utc = new Date(year, month, day, hour, minute, second, millisecond),
-    mid = new Date(year, month, day, 0, 0, 0, 0),
-    minuteOffset = -now.getTimezoneOffset(),
-    offsetCmd = (utc - mid) / 864 + Math.round(minuteOffset / 144) * 1e4,
-    local = new Date(year, month, day + offsetCmd / 1e5),
-    date  = (
-        `${local.getFullYear().toString().padStart(4, "0")}`
-        + `-${local.getMonth().toString().padStart(2, "0")}`
-        + `-${local.getDate().toString().padStart(2, "0")}`
-    );
+function myDate(year, month, day, dayOf="year") {
+    const dt = new Date(year, month - 1, day),
+    firstDay = new Date(dt.getFullYear(), 0, 1),
+    doy = (dt - firstDay) / 86400000,
+    weekdoy = doy + firstDay.getDay() - dt.getDay(),
+    week = weekdoy / 7,
+    weekFloor = Math.floor(week);
+    if (dayOf === "year") {
+        return `${dt.getFullYear()}+${Math.round(doy).toString().padStart(3, '0')}`
+    }
+    else if (dayOf === "month") {
+        return `${dt.getFullYear()}-${dt.getMonth().toString(12).toUpperCase()}-${dt.getDate().toString().padStart(2, '0')}`
+    }
+    else if (dayOf === "week") {
+        return `${dt.getFullYear()}+${weekFloor.toString().padStart(2, '0')}+${dt.getDay()}`
+    }
+    
+}
 
-console.log(date)
+console.log(myDate(2023, 12, 32, "year"));
+console.log(myDate(2023, 10, 22, "week"));
+console.log(myDate(2024, 1, 10, "week"));
+console.log(myDate(2023, 1, 8, "week"));
+console.log(myDate(2023, 12, 9, "month"));
+console.log(myDate(2022, 11, 24));
+console.log(myDate(2021, 11, 25));
+console.log(myDate(2020, 11, 26));
+console.log(myDate(2019, 11, 28));
+console.log(myDate(2018, 11, 22));
+console.log(myDate(1999, 12, 31));
+console.log(myDate(2000, 0, 34));
