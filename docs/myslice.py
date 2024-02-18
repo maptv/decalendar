1c2aa91d2a930186020feef02715122748117f94
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2024-02-18 04:11:29 -0500

    this version tolerates going out of bounds and does not have to calculate stop

--- docs/myslice.py
+++ docs/myslice.py
@@ -2,11 +2,11 @@ letters = list(map(chr, range(65, 91)))
 
 def myslice(arr, start=None, span=None, reverse=False):
     sign = not reverse or -1
-    stop = start + span * sign
-    return arr[start:stop:sign]
+    # stop = start + span * sign
+    return arr[start::sign][:span]
 
-myslice(letters, 9, 9)
-myslice(letters, 9, 9, reverse=True)
+myslice(letters, 7, 9)
+myslice(letters, 7, 9, reverse=True)
 
-myslice(letters, -10, 9)
-myslice(letters, -10, 9, reverse=True)
+myslice(letters, -7, 9)
+myslice(letters, -7, 9, reverse=True)
